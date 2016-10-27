0f168a2b299de6d05074edac1294522b649cf132
pinoyshaun1991 <shaun.brunning@talk21.com>
2016-10-27 12:57:34 +0100

    Update Dump20161027.sql

--- Dump20161027.sql
+++ Dump20161027.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `customers`
 --
 
+CREATE SCHEMA `queues` DEFAULT CHARACTER SET utf8 ;
+
 DROP TABLE IF EXISTS `customers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
